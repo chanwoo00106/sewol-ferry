0d061e821bb07a03ce4bf062937d5b127fe39eaa
변찬우 <57276315+chanwoo00106@users.noreply.github.com>
2023-04-13 14:00:16 +0900

    Fix auto scroll

--- src/components/Quize/index.tsx
+++ src/components/Quize/index.tsx
@@ -3,49 +3,49 @@ import OX from './OX'
 import Start from './Start'
 import useQuize from './store'
 import Select from './Select'
-import quizeData from '@/data/quizeData.json'
 import Complete from './Complete'
+import quizeData from '@/data/quizeData.json'
 
 const Quize = () => {
   const { question, type } = useQuize((state) => ({ ...state }))
 
   return (
-    <section
-      id='quize'
-      className='m-16 mt-20 p-10 bg-[#3C3C3C] rounded-3xl min-h-[37.5rem] flex flex-col'
-    >
-      {question === null && (
-        <>
-          <Navigation />
-          <Start />
-        </>
-      )}
-
-      {question !== null &&
-        question <= 5 &&
-        quizeData[type][question - 1].questionType === 'ox' && (
-          <OX
-            title={quizeData[type][question - 1].title}
-            question={question}
-            answer={quizeData[type][question - 1].answer}
-            commentation={quizeData[type][question - 1].commentation}
-          />
+    <>
+      <div id='quize' className='h-20' />
+      <section className='m-16 mt-20 p-10 bg-[#3C3C3C] rounded-3xl min-h-[37.5rem] flex flex-col'>
+        {question === null && (
+          <>
+            <Navigation />
+            <Start />
+          </>
         )}
 
-      {question !== null &&
-        question <= 5 &&
-        quizeData[type][question - 1].questionType === 'select' && (
-          <Select
-            title={quizeData[type][question - 1].title}
-            question={question}
-            answer={quizeData[type][question - 1].answer}
-            questions={quizeData[type][question - 1].questions as string[]}
-            description={quizeData[type][question - 1].description}
-            commentation={quizeData[type][question - 1].commentation}
-          />
-        )}
-      {question !== null && question > 5 && <Complete />}
-    </section>
+        {question !== null &&
+          question <= 5 &&
+          quizeData[type][question - 1].questionType === 'ox' && (
+            <OX
+              title={quizeData[type][question - 1].title}
+              question={question}
+              answer={quizeData[type][question - 1].answer}
+              commentation={quizeData[type][question - 1].commentation}
+            />
+          )}
+
+        {question !== null &&
+          question <= 5 &&
+          quizeData[type][question - 1].questionType === 'select' && (
+            <Select
+              title={quizeData[type][question - 1].title}
+              question={question}
+              answer={quizeData[type][question - 1].answer}
+              questions={quizeData[type][question - 1].questions as string[]}
+              description={quizeData[type][question - 1].description}
+              commentation={quizeData[type][question - 1].commentation}
+            />
+          )}
+        {question !== null && question > 5 && <Complete />}
+      </section>
+    </>
   )
 }
 
--- src/components/Think/index.tsx
+++ src/components/Think/index.tsx
@@ -1,64 +1,64 @@
 const Think = () => {
   return (
-    <section
-      id='think'
-      className='bg-[#232323] mt-20 mx-16 py-16 flex justify-center rounded-2xl'
-    >
-      <article className='w-[41rem] text-white'>
-        <img src='/svg/question.svg' className='mb-1' />
-        <h2 className='text-xl font-semibold mb-4'>생각해 봅시다.</h2>
+    <>
+      <div id='think' className='h-20' />
+      <section className='bg-[#232323] mt-20 mx-16 py-16 flex justify-center rounded-2xl'>
+        <article className='w-[41rem] text-white'>
+          <img src='/svg/question.svg' className='mb-1' />
+          <h2 className='text-xl font-semibold mb-4'>생각해 봅시다.</h2>
 
-        <h1 className='text-3xl font-bold mb-8'>
-          세월호 참사는 <span className='text-[#FDCF00]'>인권문제</span>
-          입니다.
-        </h1>
+          <h1 className='text-3xl font-bold mb-8'>
+            세월호 참사는 <span className='text-[#FDCF00]'>인권문제</span>
+            입니다.
+          </h1>
 
-        <p className='text-[#FFFFFF] opacity-50 mb-10'>
-          국가는 개인의 인권을 보장해야 합니다. 어떻게 인권을 보장할 수
-          있을까요?
-        </p>
-
-        <div className='text-[#FFFFFF80] text-xl font-medium mb-[4.5rem]'>
-          <p>
-            <span className='text-[#FDCF00]'>첫째,</span> 국가는 개인이 가진
-            <span className='text-white'>권리를 침해해서는 안 됩니다.</span>
-            이를 <span className='text-white'>불가침성</span>이라고 합니다.
+          <p className='text-[#FFFFFF] opacity-50 mb-10'>
+            국가는 개인의 인권을 보장해야 합니다. 어떻게 인권을 보장할 수
+            있을까요?
           </p>
 
-          <p>
-            <span className='text-[#FDCF00]'>둘째,</span> 개인이 다른
-            누군가로부터{' '}
-            <span className='text-white'>권리를 침해당하지 않도록 보호</span>해
-            주어야 합니다.
+          <div className='text-[#FFFFFF80] text-xl font-medium mb-[4.5rem]'>
+            <p>
+              <span className='text-[#FDCF00]'>첫째,</span> 국가는 개인이 가진
+              <span className='text-white'>권리를 침해해서는 안 됩니다.</span>
+              이를 <span className='text-white'>불가침성</span>이라고 합니다.
+            </p>
+
+            <p>
+              <span className='text-[#FDCF00]'>둘째,</span> 개인이 다른
+              누군가로부터{' '}
+              <span className='text-white'>권리를 침해당하지 않도록 보호</span>
+              해 주어야 합니다.
+            </p>
+
+            <p>
+              <span className='text-[#FDCF00]'>셋째,</span> 누군가가{' '}
+              <span className='text-white'>
+                권리를 침해당했을 때, 국가는 이를 구제
+              </span>
+              해 주어야 합니다.
+            </p>
+          </div>
+
+          <p className='text-[#FFFFFF80] mb-12'>
+            우리가 세월호 참사를 두고 정부의 책임을 묻는 것은 이 세 가지 국가의
+            의무 때문입니다. 학생들은 안전하게 여행할 권리를 누려야 했습니다.
+            그러나 해운사는 이윤에 눈이 멀어 안전 규정을 어기면서 무리하게
+            운항해 학생들의 권리를 침해했습니다. 그리고 정부는 이를 감독하지
+            못한 책임이 있습니다. 또한 위기에 처했을 때 우리는 국가로부터
+            구조받을 권리가 있습니다. 그러나 국가는 이를 제대로 보장해주지
+            못했습니다.세월호 참사도 언젠가는 우리의 뇌리에서 잊히겠지만, 수많은
+            희생자들은 잊히지 않아야 합니다.그들을 잊지 않고 그 희생의 의미를
+            기억해야합니다. 그들의 죽음은, 국가는 국민의 안전을 위해 존재해야
+            하며 인권을 보장해 주어야 한다는 사실을 너무도 아프게 일깨워줍니다.
           </p>
 
-          <p>
-            <span className='text-[#FDCF00]'>셋째,</span> 누군가가{' '}
-            <span className='text-white'>
-              권리를 침해당했을 때, 국가는 이를 구제
-            </span>
-            해 주어야 합니다.
+          <p className='text-[#FFFFFF80] text-base font-medium'>
+            - 구정화의 청소년을 위한 인권에세이에서 -
           </p>
-        </div>
-
-        <p className='text-[#FFFFFF80] mb-12'>
-          우리가 세월호 참사를 두고 정부의 책임을 묻는 것은 이 세 가지 국가의
-          의무 때문입니다. 학생들은 안전하게 여행할 권리를 누려야 했습니다.
-          그러나 해운사는 이윤에 눈이 멀어 안전 규정을 어기면서 무리하게 운항해
-          학생들의 권리를 침해했습니다. 그리고 정부는 이를 감독하지 못한 책임이
-          있습니다. 또한 위기에 처했을 때 우리는 국가로부터 구조받을 권리가
-          있습니다. 그러나 국가는 이를 제대로 보장해주지 못했습니다.세월호
-          참사도 언젠가는 우리의 뇌리에서 잊히겠지만, 수많은 희생자들은 잊히지
-          않아야 합니다.그들을 잊지 않고 그 희생의 의미를 기억해야합니다. 그들의
-          죽음은, 국가는 국민의 안전을 위해 존재해야 하며 인권을 보장해 주어야
-          한다는 사실을 너무도 아프게 일깨워줍니다.
-        </p>
-
-        <p className='text-[#FFFFFF80] text-base font-medium'>
-          - 구정화의 청소년을 위한 인권에세이에서 -
-        </p>
-      </article>
-    </section>
+        </article>
+      </section>
+    </>
   )
 }
 
